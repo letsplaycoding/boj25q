e0fdfccbd5ab7fd2510fca9053062cfdeea89d95
DESKTOP-N598GAM\동호 <alu98@naver.com>
2025-11-02 21:31:53 +0900

    사칙연산(Four Basic Operations)

---
+++ boj25q/src/Four_Basic_Operations/N1001.java
@@ -0,0 +1,17 @@
+package Four_Basic_Operations;
+
+import java.util.Scanner;
+
+public class N1001 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A - B);
+
+        sc.close();
+    }
+}
